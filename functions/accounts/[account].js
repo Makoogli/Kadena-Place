4a57970cbcd2ecc96dff2c59415e09d3fee3a6c4
Makagyn Grimm <makagyn11@gmail.com>
2023-02-20 18:16:03 -0500

    headers fix

--- functions/accounts/[account].js
+++ functions/accounts/[account].js
@@ -1,3 +1,3 @@
 export function onRequest(request) {
-	return new Response("<!DOCTYPE html><html><body><div>Your account name is "+request.params.account+"</div></body></html>",{'content-type':'text/html;charset=UTF-8'});
+	return new Response("<!DOCTYPE html><html><body><div>Your account name is "+request.params.account+"</div></body></html>",{headers:{'content-type':'text/html;charset=UTF-8'}});
 }
