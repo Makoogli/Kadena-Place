9505d078b190e71ec7e9393d5ce0d676eef7e3a8
Makagyn Grimm <makagyn11@gmail.com>
2023-02-20 18:14:15 -0500

    onRequest

--- functions/accounts/[account].js
+++ functions/accounts/[account].js
@@ -1,3 +1,3 @@
-export function handleRequest(request) {
+export function onRequest(request) {
 	return new Response("<!DOCTYPE html><html><body><div>Your account name is "+request.params.account+"</div></body></html>",{'content-type':'text/html;charset=UTF-8'});
 }
