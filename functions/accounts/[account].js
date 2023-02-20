ca23b8329f3bfda3646806ed1937ed9011432417
Makagyn Grimm <makagyn11@gmail.com>
2023-02-20 18:46:49 -0500

    onRequest to fetch

--- functions/accounts/[account].js
+++ functions/accounts/[account].js
@@ -1,18 +1,17 @@
-export async function onRequest(request) {
-	class ElementHandler {
-	  element(element) {
-	    // An incoming element, such as `div`
-	    element.setInnerContent("account is "+request.params.account);
-	  }
+export default {
+	async fetch(request){
+		class ElementHandler {
+			element(element) {
+	    		element.setInnerContent("account is "+request.params.account);
+			}
 
-	  comments(comment) {
-	    // An incoming comment
-	  }
+			comments(comment) {
+			}
 
-	  text(text) {
-	    // An incoming piece of text
-	  }
+			text(text) {
+			}
+		}
+		let res = await fetch(request);
+		return new HTMLRewriter().on('div', new ElementHandler()).transform(res);
 	}
-	let res = await fetch(request);
-	return new HTMLRewriter().on('div', new ElementHandler()).transform(res);
 }
