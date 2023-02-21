2ad3e842e0c936921447a245487b01e61575c5a5
Makagyn Grimm <makagyn11@gmail.com>
2023-02-21 12:11:18 -0500

    test without try

--- dist/accounts.js
+++ dist/accounts.js
@@ -1,9 +1,9 @@
 $(document).ready(async function(){
-	let res;
+	let res = JSON.stringify(await KadenaPlace.getKPAccount());/*
 	try{
 		res = JSON.stringify(await KadenaPlace.getKPAccount());
 	}catch{
 		res = "Does not exist";
-	}
+	}*/
 	$("body").text(res);
 });
