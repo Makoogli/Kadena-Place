8ed6701f87a8fdd93f3987a23c6b2a8dab0e380b
Makagyn Grimm <makagyn11@gmail.com>
2023-02-21 12:21:15 -0500

    fix error

--- dist/accounts.js
+++ dist/accounts.js
@@ -1,10 +1,8 @@
 $(document).ready(async function(){
-	let res = JSON.stringify(await KadenaPlace.getKPAccount(url_param_account));/*
-	try{
-		res = JSON.stringify(await KadenaPlace.getKPAccount());
-	}catch{
+	let res = JSON.stringify(await KadenaPlace.getKPAccount(url_param_account));
+	if(res == undefined){
 		res = "Does not exist";
-	}*/
+	}
 	console.log(res);
 	$("body").text(res);
 });
