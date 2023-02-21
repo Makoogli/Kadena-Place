a276054a5689f80f870c9c65b69cef6345ec5d8d
Makagyn Grimm <makagyn11@gmail.com>
2023-02-20 22:21:47 -0500

    fix inject js

--- functions/accounts/[account].js
+++ functions/accounts/[account].js
@@ -2,7 +2,7 @@ export async function onRequest(request) {
 	class ElementHandler {
 	  element(element) {
 	    // An incoming element, such as `div`
-	    element.before("<head><script>let url_param_account = '"+request.params.account+"';</script></head>");
+	    element.text("let url_param_account = '"+request.params.account+"';");
 	  }
 
 	  comments(comment) {
@@ -14,5 +14,5 @@ export async function onRequest(request) {
 	  }
 	}
 	let res = await fetch('https://dynamicaccounts.kadena-place.pages.dev/accounts.html');
-	return new HTMLRewriter().on('head', new ElementHandler()).transform(res);
+	return new HTMLRewriter().on('script#set_url_param_account_variable', new ElementHandler()).transform(res);
 }
