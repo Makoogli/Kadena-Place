c6b51f2df3e5a1fbe434cc422f7914796701e676
Makagyn Grimm <makagyn11@gmail.com>
2023-03-04 03:18:29 -0500

    fix .joined to .joined.timep

--- dist/timeline.js
+++ dist/timeline.js
@@ -4,5 +4,5 @@ function timepToMilliseconds(timep){
 
 $(document).ready(async function(){
 	let placeHistory = await KadenaPlace.placeHistory();
-	$("body").text((timepToMilliseconds((await KadenaPlace.getKPAccount("k:e61108b15a8c5b45d7593ebdd1e66c1bd0d9f40cb4190d2e68c922355c0bb932")).joined)).toString());
+	$("body").text((timepToMilliseconds((await KadenaPlace.getKPAccount("k:e61108b15a8c5b45d7593ebdd1e66c1bd0d9f40cb4190d2e68c922355c0bb932")).joined.timep)).toString());
 });
