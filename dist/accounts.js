dbe1d1c4a509b764a5846650aaa25314def61bb9
Makagyn Grimm <makagyn11@gmail.com>
2023-03-13 01:44:53 -0400

    accounts.js to support empty param

--- dist/accounts.js
+++ dist/accounts.js
@@ -1,8 +1,12 @@
 $(document).ready(async function(){
-	let res = JSON.stringify(await KadenaPlace.getKPAccount(url_param_account));
-	if(res == undefined){
-		res = "Does not exist";
+	try{
+		let res = JSON.stringify(await KadenaPlace.getKPAccount(url_param_account));
+		if(res == undefined){
+			res = "Does not exist";
+		}
+		console.log(res);
+		$("body").text(res);
+	}catch{
+		$("body").text("accounts");
 	}
-	console.log(res);
-	$("body").text(res);
 });
